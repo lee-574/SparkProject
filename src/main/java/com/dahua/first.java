841586af4de4ca840dae2f37fd2b2ec23fc84a32
lee-574 <penghui574@sina.com>
2021-04-15 19:58:15 +0800

    第一次提交

---
+++ src/main/java/com/dahua/first.java
@@ -0,0 +1,13 @@
+package com.dahua;
+
+/**
+ * @author SuDef
+ * @version 1.0
+ * @date 2021/4/15 19:56
+ */
+public class first {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
